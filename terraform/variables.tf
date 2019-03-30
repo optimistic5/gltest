c7e5c7ba5e4a45b5c517c37565c896c39f055e89
Aleksandr Vasiutin <vasiutin.aleksandr@gmail.com>
2019-03-30 18:38:42 +0200

    Added network and sg

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,3 +13,41 @@ variable "amis" {
 variable "instance_type" {
   default = "t2.micro"
 }
+
+variable "tag_name" {
+  default = "Windows Server 2016"
+}
+
+######Network
+
+variable "web_port" {
+  default = "8080"
+}
+
+variable "rdp_port" {
+  default = "3389"
+}
+
+variable "count" {
+  default = "2"
+}
+
+variable "av_zone" {
+  default =  [ "eu-central-1a", "eu-central-1b"]
+}
+
+variable "network_name" {
+  default = "glnetwork"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  default = ["10.0.10.0/24", "10.0.20.0/24"]
+}
+
+variable "private_ip" {
+  default = ["10.0.10.100", "10.0.20.100"]
+}
