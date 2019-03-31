a6cbe70b0aeb1d48e1eb0fe4d9133cfeccb8d1f4
Aleksandr Vasiutin <vasiutin.aleksandr@gmail.com>
2019-03-31 11:25:27 +0300

    Added gw and routes to network

--- terraform/network.tf
+++ terraform/network.tf
@@ -16,3 +16,29 @@ resource "aws_subnet" "glnetwork_subnet" {
     Name = "${var.network_name}-${count.index +1}"
   }
 }
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.glnetwork.id}"
+
+  tags = {
+    Name = "${var.network_name}-gw"
+  }
+}
+
+resource "aws_route_table" "route_table" {
+  vpc_id = "${aws_vpc.glnetwork.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+  tags {
+    Name = "route_table"
+  }
+}
+
+resource "aws_route_table_association" "rt_a" {
+  count = "${length(var.subnet_cidr)}"
+  subnet_id      = "${element(aws_subnet.glnetwork_subnet.*.id,count.index)}"
+  route_table_id = "${aws_route_table.route_table.id}"
+}
+
